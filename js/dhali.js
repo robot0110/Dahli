d0d73a98855e6c4d6a1e08ac246cf6231fb4899f
U-INETWEBUSA\cole.s <cole.s@Web5.inetwebusa.com>
2016-10-14 09:00:20 -0500

    refactored code to allow for additional options

--- js/dhali.js
+++ js/dhali.js
@@ -1,29 +1,105 @@
 'using strict';
 
 
-var	mid,
-	w,
-	isHidden = []
+var dali = {
+	elements: [],
+	config: function(options){
 
-$.each( $('.dhali'), function () {
-
-	isHidden.push(this)
-})
-
-
-mid = $(window).height() / 2 
-
-
-w = $(window).scroll(function() {
-	for (i = 0; i < isHidden.length; i++){
-		if ( w.scrollTop() >= $(isHidden[i]).offset().top - mid ) {
-
-			$(isHidden[i]).addClass( $(isHidden[i]).attr('data-animation') )
+		if (!options){
+			this.custom(options)
+		} else {
+			this.getScreen()
+			this.loadArray()
+			this.setAnimationSpeed()
+			this.watch()
 		}
-		
-		//Reverse transition if scroll goes back up?
+	},
+	getScreen: function(){
+		this.mid = $(window).height() / 2
+	},
+	loadArray: function() {
+
+		var that = this
+
+		$.each( $('[dali]'), function() {
+
+			var options = that.buildOptions(this)
+
+			var dali = {
+				el: this,
+				transition: options['transition'],
+				speed: options['speed']
+			}
+
+			that.elements.push(dali)
+		})
+	},
+	buildOptions: function(dali) {
+		var data = $(dali).attr('dali'),
+			splitData = data.split(','),
+			options = {
+				transition: 'fadeInUp',
+				speed: '1s'
+			}
+
+		if ( splitData.length > 1){
+			options['transition'] = splitData[0]
+			options['speed'] = splitData[1]
+		} else {
+			options['transition'] = data
+		}
+
+		return options
+	},
+	setAnimationSpeed: function(){
+		var elements = this.elements
+
+		for (var i = 0; i < elements.length; i++) {
+
+			var speed = elements[i].speed
+
+			$(elements[i].el).css({
+			 	'-webkit-animation-duration': speed,
+			 	'-moz-animation-duration':    speed,
+			 	'-ms-animation-duration':     speed,
+			 	'-o-animation-duration':      speed,
+			 	'animation-duration':         speed     
+			})
+		}
+	},
+	watch: function() {
+		var that = this
+
+		var r = $(window).resize(function() {
+			that.getScreen()
+		})
+
+		var w = $(window).scroll(function() {
+			that.checkElement(w)
+		})
+	},
+	checkElement: function(w){
+		var that = this
+		var elements = that.elements
+
+		for (i = 0; i < elements.length; i++){
+				if ( w.scrollTop() >= $(elements[i].el).offset().top - that.mid ) {
+					$(elements[i].el).addClass( elements[i].transition )
+				}
+			}
+	},
+	custom: function(options) {
+
+		//set attr of all items of one class or id
+		//change the name of the attr to run the animation
+
+		//at the end run the default config to finish loading
+		this.config()
 	}
-})
+
+}
+
+dali.config()
 
 
 
@@ -31,12 +107,5 @@ w = $(window).scroll(function() {
 
 
 
-// function Dhali (el, transition, speed) {
 
-// 	this.el = el;
-// 	this.transition = transition;
-// 	this.speed = speed;
 
-// }
-
-// var dhali = new Dhali()
